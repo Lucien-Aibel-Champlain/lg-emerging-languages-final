d03763cf09c6841dce7d3eda045215df004e4e9c
Lucien Rohwein Aibel <lucien.aibel@mymail.champlain.edu>
2026-03-31 19:11:20 -0400

    Now loads training data on start.

--- knn-classifier/src/main.rs
+++ knn-classifier/src/main.rs
@@ -1,31 +1,21 @@
 use image::{ImageReader, DynamicImage, GenericImageView, Pixel, Rgb};
-use std::io::BufReader;
+use std::io::BufReader; //TODO switch accesseses to be io:: in program, not up here.
 use std::fs;
-
-enum Digit {
-    Zero,
-    One,
-    Two,
-    Three,
-    Four,
-    Five,
-    Six,
-    Seven,
-    Eight,
-    Nine,
-}
 
 const MNIST_WIDTH: usize = 28;
 const MNIST_HEIGHT: usize = MNIST_WIDTH;
 
 struct MNISTImage {
     data: [u8; MNIST_WIDTH * MNIST_HEIGHT],
-
 }
 
 struct ClassedImage {
     image: MNISTImage,
-    class: Digit,
+    class: u8,
+}
+
+struct TrainingData {
+    dataset: Vec<ClassedImage>,
 }
 
 impl MNISTImage {
@@ -35,17 +25,17 @@ impl MNISTImage {
         }
     }
 
-    fn from_file(filename: &str) -> Result<MNISTImage, &'static str> {
+    fn from_file(filename: &str) -> Result<MNISTImage, String> {
         match ImageReader::open(filename) {
             Ok(reader) => Self::from_imagereader(reader),
-            Err(_) => Err("Error opening image file."),
+            Err(_) => Err("Error opening image file.".to_string()),
         }
     }
 
-    fn from_imagereader(reader: ImageReader<BufReader<fs::File>>) -> Result<MNISTImage, &'static str> {
+    fn from_imagereader(reader: ImageReader<BufReader<fs::File>>) -> Result<MNISTImage, String> {
         match reader.decode() {
             Ok(imagedata) => Self::from_dynamicimage(imagedata),
-            Err(msg) => Err("Error decoding image file."),
+            Err(_) => Err("Error decoding image file.".to_string()),
         }
     }
 
@@ -61,7 +51,7 @@ impl MNISTImage {
         }
     }
 
-    fn from_dynamicimage(image_data: DynamicImage) -> Result<MNISTImage, &'static str> {
+    fn from_dynamicimage(image_data: DynamicImage) -> Result<MNISTImage, String> {
         let mut i = 0;
         let mut new_image = MNISTImage::blank();
         for pix in image_data.pixels() {
@@ -91,10 +81,68 @@ impl MNISTImage {
     }
 }
 
-fn main() {
-    let img = MNISTImage::from_file("mnist_png/train/0/1.png");
-    match img {
-        Ok(img) => img.print(),
-        Err(msg) => println!("{}",msg),
+impl TrainingData {
+    fn new() -> TrainingData {
+        TrainingData {
+            dataset: Vec::new()
+        }
     }
+
+    fn from_directory(directory: &str) -> Result<TrainingData, String> {
+        let training_directory = directory.to_owned() + "/train/";
+        
+        let mut new_trainingdata = TrainingData::new();
+
+        for digit in 0..=9 {
+            //Create a new string (cloned from the general training string) to point to this digit's subfolder
+            let current_directory = training_directory.clone() + &digit.to_string();
+
+            //Get a reader to list all entries inside current_directory
+            let reader = match fs::read_dir(&current_directory) {
+                Ok(unpacked_reader) => unpacked_reader,
+                Err(_) => return Err(format!("Cannot open {} for reading", current_directory)),
+            };
+
+            for entry in reader {
+                //Check whether entry exists
+                let entry = match entry {
+                    Ok(unpacked_entry) => unpacked_entry,
+                    Err(_) => return Err(format!("Error unpacking entry under {}", current_directory)),
+                };
+
+                //Convert the DirEntry to a string path to the file in question
+                let path = match entry.path().to_str() {
+                    Some(unpacked_path) => unpacked_path.to_string(), //.to_str gives a slice which will be lost when the result of entry.path gets deallocated, so we convert it to a String
+                    None => return Err(format!("Cannot convert path {} to UTF-8 String", entry.path().display())),
+                };
+
+                //Read in the image data
+                let img = match MNISTImage::from_file(&path) {
+                    Ok(unpacked_image) => unpacked_image,
+                    Err(string) => return Err(string),
+                };
+
+                //Add the new image to the end of the new dataset
+                new_trainingdata.dataset.push(ClassedImage {
+                    image: img,
+                    class: 0,
+                });
+            }
+        }
+
+        Ok(new_trainingdata)
+    }
+}
+
+const DATASET_DIRECTORY: &str = "minimalmnist";
+
+fn main() {
+    let dataset = match TrainingData::from_directory(DATASET_DIRECTORY) {
+        Ok(dataset) => dataset,
+        Err(msg) => {
+            println!("Error loading data: {}",msg);
+            return;
+        }
+    };
+    println!("Data imported successfully! Length: {}", dataset.dataset.len());
 }
