1cfca3d77d4127f65dd90a250ff0dee296780330
Lucien Rohwein Aibel <lucien.aibel@mymail.champlain.edu>
2026-04-05 22:40:23 -0400

    Sequential algorithm complete.

--- knn-classifier/src/main.rs
+++ knn-classifier/src/main.rs
@@ -81,17 +81,12 @@ impl MNISTImage {
     }
 
     fn calculate_distance(&self, other: &MNISTImage) -> f64 {
-
-
-
         let mut accumulator: f64 = 0f64;
         for i in 0..(MNIST_WIDTH * MNIST_HEIGHT){
             accumulator += ((self.data[i] as i16 - other.data[i] as i16) as f64).powf(2f64);
         }
         return accumulator.sqrt()
     }
-
-
 }
 
 impl TrainingData {
@@ -145,9 +140,108 @@ impl TrainingData {
 
         Ok(new_trainingdata)
     }
+
+    fn classify(&self, image: &MNISTImage, k: u32) -> Result<u8, String> {
+        let k = if let Ok(k) = usize::try_from(k) { k } else { return Err(format!("k of {} is larger than maximum array size on this platform.", k)) };
+        let mut lowest_distances = vec![(f64::INFINITY,0u8); k];
+
+        for training_img in &self.dataset {
+            let distance = image.calculate_distance(&training_img.image);
+            place_in_vector(distance, &mut lowest_distances, training_img.class);
+        }
+
+        Ok(take_votes(lowest_distances))
+    }
 }
 
-const DATASET_DIRECTORY: &str = "minimalmnist";
+fn take_votes(lowest_distances: Vec<(f64, u8)>) -> u8{
+    const NUM_CLASSES: u8 = 10;
+    let mut votes = [0; NUM_CLASSES as usize];
+    for (dist, class) in lowest_distances {
+        votes[usize::from(class)] += 1;
+    }
+
+    let mut highest = (0, 0);
+    for i in 0..NUM_CLASSES {
+        let this_vote = votes[usize::from(i)];
+        if this_vote > highest.0 {
+            highest = (this_vote, i);
+        }
+    }
+    highest.1
+}
+
+fn place_in_vector(val: f64, vector: &mut Vec<(f64, u8)>, class: u8) {
+    if val >= vector[0].0 {
+        return;
+    }
+    let len = vector.len();
+    for i in 1..len {
+        vector[i - 1] = vector[i];
+        if val > vector[i].0 {
+            vector[i - 1] = (val, class);
+            return;
+        }
+    }
+    vector[len - 1] = (val, class);
+}
+
+fn test(model: TrainingData, k: u32, data_directory: &str, verbose: bool) -> Result<f64, String> {
+    let mut successes = 0;
+    let mut total = 0;
+    let test_directory = data_directory.to_owned() + "/test/";
+
+    for digit in 0..=9 {
+        //Create a new string (cloned from the general training string) to point to this digit's subfolder
+        let current_directory = test_directory.clone() + &digit.to_string();
+
+        //Get a reader to list all entries inside current_directory
+        let reader = match fs::read_dir(&current_directory) {
+            Ok(unpacked_reader) => unpacked_reader,
+            Err(_) => return Err(format!("Cannot open {} for reading", current_directory)),
+        };
+        
+        for entry in reader {
+            //Check whether entry exists
+            let entry = match entry {
+                Ok(unpacked_entry) => unpacked_entry,
+                Err(_) => return Err(format!("Error unpacking entry under {}", current_directory)),
+            };
+
+            //Convert the DirEntry to a string path to the file in question
+            let path = match entry.path().to_str() {
+                Some(unpacked_path) => unpacked_path.to_string(), //.to_str gives a slice which will be lost when the result of entry.path gets deallocated, so we convert it to a String
+                None => return Err(format!("Cannot convert path {} to UTF-8 String", entry.path().display())),
+            };
+
+            //Read in the image data
+            let img = match MNISTImage::from_file(&path) {
+                Ok(unpacked_image) => unpacked_image,
+                Err(string) => return Err(string), //Our functions are already set up to return user-readable Strings, so we don't need to make one up like we did for the external functions
+            };
+
+            let class = match model.classify(&img, k) {
+                Ok(class) => class,
+                Err(string) => return Err(string),
+            };
+
+            if class == digit {
+                successes += 1;
+            }
+            if verbose {
+                match class == digit {
+                    true => println!("Succesfully classed {}.", path),
+                    false => println!("MISS on test case {}. Expected result {}, got {}.", path, digit, class),
+                }
+            }
+            total += 1;
+        }
+    }
+
+    Ok(f64::from(successes) / f64::from(total))
+}
+
+const DATASET_DIRECTORY: &str = "mnist_png";
 
 fn main() {
     let dataset = match TrainingData::from_directory(DATASET_DIRECTORY) {
@@ -159,4 +253,9 @@ fn main() {
     };
     println!("Data imported successfully! Length: {}", dataset.dataset.len());
 
+    println!("Beginning testing ...");
+    match test(dataset, 4, DATASET_DIRECTORY, true) {
+        Ok(score) => println!("Test complete.\nAccuracy: {}%", score * 100.0),
+        Err(string) => println!("Error while testing: {}",string),
+    }
 }
